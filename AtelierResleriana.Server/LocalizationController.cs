eb580fcd1107fee527b4aabb2b92239f60e1c2cc
Andrew Ong <ong.andrew@gmail.com>
2025-05-31 01:47:33 +1000

    Master data QA.

--- AtelierResleriana.Server/LocalizationController.cs
+++ AtelierResleriana.Server/LocalizationController.cs
@@ -12,8 +12,8 @@ namespace AtelierResleriana.Server
             {                
                 new LocalizationDataVersion()
                 {
-                    Version = 638842111064594601,
-                    Uri = new Uri("https://atelierresleriana.blob.core.windows.net/localization/638842111064594601.zip")
+                    Version = 638842166069800230,
+                    Uri = new Uri("https://atelierresleriana.blob.core.windows.net/localization/638842166069800230.zip")
                 }
             });
         }

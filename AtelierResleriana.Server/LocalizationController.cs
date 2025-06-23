0fb070190372c02d0f23bf427235de67fe75c3d3
Andrew Ong <ong.andrew@gmail.com>
2025-06-23 14:32:09 +1000

    Meruru 6 star.

--- AtelierResleriana.Server/LocalizationController.cs
+++ AtelierResleriana.Server/LocalizationController.cs
@@ -12,8 +12,8 @@ namespace AtelierResleriana.Server
             {                
                 new LocalizationDataVersion()
                 {
-                    Version = 638853916375002987,
-                    Uri = new Uri("https://atelierresleriana.blob.core.windows.net/localization/638853916375002987.zip")
+                    Version = 638862498031984927,
+                    Uri = new Uri("https://atelierresleriana.blob.core.windows.net/localization/638862498031984927.zip")
                 }
             });
         }

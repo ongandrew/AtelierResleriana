3f40ca8a324ab2f42e6d339480d7475e2054a93d
Andrew Ong <ong.andrew@gmail.com>
2025-07-22 13:59:10 +1000

    News and minor fixes for summer.

--- AtelierResleriana.Server/LocalizationController.cs
+++ AtelierResleriana.Server/LocalizationController.cs
@@ -12,8 +12,8 @@ namespace AtelierResleriana.Server
             {                
                 new LocalizationDataVersion()
                 {
-                    Version = 638878368203534896,
-                    Uri = new Uri("https://atelierresleriana.blob.core.windows.net/localization/638878368203534896.zip")
+                    Version = 638887531944069518,
+                    Uri = new Uri("https://atelierresleriana.blob.core.windows.net/localization/638887531944069518.zip")
                 }
             });
         }

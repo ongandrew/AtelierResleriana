1961e2de7711d74777c03057dde608a46d318a28
Andrew Ong <ong.andrew@gmail.com>
2025-11-07 18:37:20 +1100

    Crow release.

--- AtelierResleriana.Server/LocalizationController.cs
+++ AtelierResleriana.Server/LocalizationController.cs
@@ -12,8 +12,8 @@ namespace AtelierResleriana.Server
             {                
                 new LocalizationDataVersion()
                 {
-                    Version = 638976043468640421,
-                    Uri = new Uri("https://atelierresleriana.blob.core.windows.net/localization/638976043468640421.zip")
+                    Version = 638980975973361653,
+                    Uri = new Uri("https://atelierresleriana.blob.core.windows.net/localization/638980975973361653.zip")
                 }
             });
         }

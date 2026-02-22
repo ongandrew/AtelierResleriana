f6ecfb0bb509537601a4a86a43b7a99ddd70a3f9
Andrew Ong <ong.andrew@gmail.com>
2026-02-22 19:47:28 +1100

    Skill name alignment.

--- AtelierResleriana.Server/LocalizationController.cs
+++ AtelierResleriana.Server/LocalizationController.cs
@@ -12,8 +12,8 @@ namespace AtelierResleriana.Server
             {                
                 new LocalizationDataVersion()
                 {
-                    Version = 639073451748340693,
-                    Uri = new Uri("https://atelierresleriana.blob.core.windows.net/localization/639073451748340693.zip")
+                    Version = 639073467736018218,
+                    Uri = new Uri("https://atelierresleriana.blob.core.windows.net/localization/639073467736018218.zip")
                 }
             });
         }
